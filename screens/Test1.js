bd33bd54988b839378bc6f25b23b6a35eac4f2d9
jmcasebier <jason.beercase@gmail.com>
2019-03-09 15:46:36 -0600

    updated test1 screen for simplicity

--- screens/Test1.js
+++ screens/Test1.js
@@ -9,7 +9,6 @@ export default class Test1 extends React.Component {
 
   state = {
     location: null,
-    address: null,
   };
 
   _getLocationAsync = async() => {
@@ -19,18 +18,8 @@ export default class Test1 extends React.Component {
       return;
     }
     let location = await Location.getCurrentPositionAsync({});
-    console.log("Location updated.");
-    try {
-      let address = await Location.reverseGeocodeAsync({
-      latitude: this.state.location.coords.latitude,
-      longitude: this.state.location.coords.longitude});
-      this.setState({location: location, address: address});
-      console.log("Address updated.");
-      return;
-    } catch {
-      console.log("Reverse geocode failed!");
-    }
     this.setState({location: location});
+    console.log("Location updated.");
   }
 
   componentDidMount() {
@@ -40,10 +29,10 @@ export default class Test1 extends React.Component {
   render() {
     if (!this.state.location) {
       return (<View />);
-    } else if (this.state.location && !this.state.address) {
+    } else {
       return (
         <View style={{flex: 1}}>
-        <MapView style={{flex: 7}}
+        <MapView style={{flex: 1}}
           region={{
             latitude: this.state.location.coords.latitude,
             longitude: this.state.location.coords.longitude,
@@ -53,62 +42,8 @@ export default class Test1 extends React.Component {
           showsUserLocation={true}
           showsMyLocationButton={false}
         />
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center',
-          borderTopWidth: 1, borderTopColor: '#3333'}}>
-          <Button title="Get Address" onPress={this._getLocationAsync} />
-        </View>
         </View>
       );
-    } else {
-      let cityStateZip = this.state.address[0].city + ", " + this.state.address[0].region
-        + " " + this.state.address[0].postalCode;
-      if (this.state.address[0].name.length + this.state.address[0].street.length + 1 <= 28) {
-        let streetAddress = this.state.address[0].name + " " + this.state.address[0].street;
-        return (
-          <View style={{flex: 1}}>
-          <MapView style={{flex: 4}}
-            region={{
-              latitude: this.state.location.coords.latitude,
-              longitude: this.state.location.coords.longitude,
-              latitudeDelta: 0.002,
-              longitudeDelta: 0.0008,
-            }}
-            showsUserLocation={true}
-            showsMyLocationButton={false}
-          />
-          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center',
-            borderTopWidth: 1, borderTopColor: '#3333', paddingBottom: 8}}>
-            <Text style={{fontSize: 20, fontWeight: 'bold', color: '#333a'}}>{streetAddress}</Text>
-            <Text style={{fontSize: 15, marginBottom: 12, color: '#333a'}}>{cityStateZip}</Text>
-            <Button title="Update Address" onPress={this._getLocationAsync} />
-          </View>
-          </View>
-        );
-      } else {
-        let nameAddress = this.state.address[0].name;
-        let streetAddress = this.state.address[0].street;
-        return (
-          <View style={{flex: 1}}>
-          <MapView style={{flex: 4}}
-            region={{
-              latitude: this.state.location.coords.latitude,
-              longitude: this.state.location.coords.longitude,
-              latitudeDelta: 0.002,
-              longitudeDelta: 0.0008,
-            }}
-            showsUserLocation={true}
-            showsMyLocationButton={false}
-          />
-          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center',
-            borderTopWidth: 1, borderTopColor: '#3333', paddingBottom: 8}}>
-            <Text style={{fontSize: 18, fontWeight: 'bold', color: '#333a'}}>{nameAddress}</Text>
-            <Text style={{fontSize: 20, fontWeight: 'bold', color: '#333a'}}>{streetAddress}</Text>
-            <Text style={{fontSize: 15, marginBottom: 12, color: '#333a'}}>{cityStateZip}</Text>
-            <Button title="Update Address" onPress={this._getLocationAsync} />
-          </View>
-          </View>
-        );
-      }
     }
   }
 }
